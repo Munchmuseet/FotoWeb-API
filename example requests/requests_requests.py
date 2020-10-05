07c2a0aced1fd7c47d11287e5d2408a274b2e041
Christer Vinje Gimse <68919499+drop0x55@users.noreply.github.com>
2020-10-05 12:11:53 +0200

    Create requests_requests.py

---
+++ example requests/requests_requests.py
@@ -0,0 +1,116 @@
+# GET /fotoweb/
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.api-descriptor+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives/{archive}
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives/5014-Malerier"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.assetlist+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives/{archive}/Arkiv/{filename}.info
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives/5014-Malerier/Arkiv/M0112_20190403.tif.info"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.asset+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.collectionlist+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives/{archive}
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives/5014-Malerier"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.collection+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives/{archive}
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives/5014-Malerier"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.collectioninfo+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
+
+# GET /fotoweb/archives/{archive}/?{field name}={field content}
+
+import requests
+
+url = "https://foto.munchmuseet.no/fotoweb/archives/5014-Malerier/?105=vampire"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.fotoware.assetlist+json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
